e47be26aa05c679677977ca7d5ad51bf34db3757
LittleHaku <ivandelhorno@gmail.com>
2023-09-12 20:57:54 +0300

    unit testing

---
+++ src/tests/test_reversi.py
@@ -0,0 +1,66 @@
+# Make some unit testing for the reversi game
+import unittest
+from reversi import ReversiGame
+
+
+class TestReversiGame(unittest.TestCase):
+
+    def setUp(self):
+        # Create a new instance of ReversiGame for each test case
+        self.game = ReversiGame()
+
+    def test_initial_board_setup(self):
+        # Test that the initial board setup is correct
+        expected_board = [
+            [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+            [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+            [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+            [' ', ' ', ' ', 'W', 'B', ' ', ' ', ' '],
+            [' ', ' ', ' ', 'B', 'W', ' ', ' ', ' '],
+            [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+            [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+            [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+        ]
+        self.assertEqual(self.game.board, expected_board)
+        self.assertEqual(self.game.current_player, 'W')
+
+    def test_valid_moves(self):
+        # Test the get_valid_moves method
+        valid_moves = self.game.get_valid_moves()
+        
+        # Since the game starts with the white player, the valid moves are:
+        expected_valid_moves = [(2, 4), (3, 5), (4, 2), (5, 3)]
+
+        # (order doesn't matter)
+        for move in valid_moves:
+            self.assertIn(move, expected_valid_moves)
+
+    def test_make_move(self):
+        # Test the make_move method
+        # First with a valid move
+        self.assertTrue(self.game.make_move(2, 4))
+        # Check the piece is placed
+        self.assertEqual(self.game.board[2][4], 'W')
+
+        # Then with an invalid move (already occupied cell)
+        self.assertFalse(self.game.make_move(2, 4))
+        # Check the board is not modified
+        self.assertEqual(self.game.board[2][4], 'W')
+
+    def test_winner(self):
+        # Test the get_winner method
+        self.game.board = [
+            ['B', 'B', 'B', 'B', 'B', 'B', 'B', 'B'],
+            ['B', 'B', 'B', 'B', 'B', 'B', 'B', 'B'],
+            ['B', 'B', 'B', 'B', 'B', 'B', 'B', 'B'],
+            ['B', 'B', 'B', 'B', 'B', 'B', 'B', 'B'],
+            ['B', 'B', 'B', 'B', 'B', 'B', 'B', 'B'],
+            ['B', 'B', 'B', 'B', 'B', 'B', 'B', 'B'],
+            ['B', 'B', 'B', 'B', 'B', 'B', 'B', 'B'],
+            ['B', 'B', 'B', 'B', 'B', 'B', 'B', 'B']
+        ]
+        self.assertEqual(self.game.get_winner(), 'B')
+
+
+if __name__ == '__main__':
+    unittest.main()
