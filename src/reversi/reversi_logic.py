56eeeb6f370d7493a54fa1328b29bd9177efc4e5
LittleHaku <ivandelhorno@gmail.com>
2023-09-20 18:05:05 +0300

    Fixed undo!!!! PYTHON AND HIS POINTERS OMG

--- src/reversi/reversi_logic.py
+++ src/reversi/reversi_logic.py
@@ -6,7 +6,7 @@ File containing the logic implementation of the game
 class ReversiGame:
     """Reversi Game Class"""
 
-    def __init__(self, board=None, current_player=None):
+    def __init__(self, board=None, current_player=None, move_stack=None):
         """Constructor, initializes the game with a board of 8x8 and the
         initial pieces in the center, also sets the current player to white"""
         self.board_size = 8
@@ -14,18 +14,33 @@ class ReversiGame:
             [" " for _ in range(self.board_size)]
             for _ in range(self.board_size)
         ]
-        # Initialize an empty stack to store previous game states
-        self.move_stack = []
+
         # Initial pieces
-        if board is None and current_player is None:
+        if board is None and current_player is None and move_stack is None:
             self.board[3][3] = "W"
             self.board[3][4] = "B"
             self.board[4][3] = "B"
             self.board[4][4] = "W"
+            # Board to test if AI tries to win
+            """ self.board = [
+                ['B', 'W', 'B', 'B', 'B', 'B', ' ', ' '],
+                ['B', 'B', 'B', 'B', 'B', 'B', 'B', 'B'],
+                ['B', 'B', 'B', 'B', 'B', 'B', 'B', 'B'],
+                ['B', 'B', 'B', 'B', 'B', 'B', 'B', 'B'],
+                ['B', 'B', 'B', 'B', 'B', 'B', 'B', 'B'],
+                ['B', 'B', 'B', 'B', 'B', 'B', 'B', 'B'],
+                ['B', 'B', 'B', 'B', 'W', 'B', 'B', 'B'],
+                ['B', 'B', 'B', 'B', ' ', 'B', 'B', 'B']
+            ] """
             self.current_player = "W"
+            # Start the stack with the initial game state
+            self.move_stack = [
+                ([row[:] for row in self.board], self.current_player)
+            ]
         else:
             self.board = board
             self.current_player = current_player
+            self.move_stack = move_stack
 
     def is_valid_move(self, row, col):
         """Checks if the move to a certain cell is valid or not, it will check
@@ -88,14 +103,25 @@ class ReversiGame:
         flip the corresponding pieces and update the current player (returns
         True), otherwise it will return False"""
 
-        print("Move: ", row, col)
+        # print("Move: ", row, col)
         if self.is_valid_move(row, col):
             # print("Valid Move")
             self.board[row][col] = self.current_player
             self.flip_pieces(row, col)
             self.current_player = self.opponent()
             # After making a move, push the current game state onto the stack
-            self.move_stack.append((self.board, self.current_player))
+
+            # print("pushing onto stack")
+            self.move_stack.append(([row[:]
+                                    for row in self.board],
+                                    self.current_player))
+
+            # Print the stack
+            """ print("Stack: ")
+            for state in self.move_stack:
+                for row in state[0]:
+                    print(row)
+                print(state[1]) """
             return True
         else:
             return False
@@ -178,7 +204,9 @@ class ReversiGame:
 
         # If the game is over or the depth is 0 then return the evaluation
         if self.is_game_over() or depth == 0:
-            return self.evaluate_board(), None
+            evaluation = self.evaluate_board()
+            print(f"Evaluated Board at depth {depth}: {evaluation}")
+            return evaluation, None
 
         valid_moves = self.get_valid_moves()
 
@@ -191,63 +219,113 @@ class ReversiGame:
                 row, col = move
                 # Make the move
                 self.make_move(row, col)
+                print(f"Maximizing Player: Trying move {move}")
+                # Print the board after making the move
+                """ for row in self.board:
+                    print(row) """
                 # Recursively call minimax for the next level
                 evaluation, _ = self.alphabeta_minimax(
                     depth - 1, False, alpha, beta)
+                print(
+                    f"Maximizing Player: Evaluated move {move} to {evaluation}")
                 # Undo the move
                 self.undo_move()
 
                 if evaluation > max_eval:
-                    best_eval = evaluation
+                    max_eval = evaluation
                     best_move = move
 
                 # Take the better between current eval and current alpha
                 alpha = max(alpha, evaluation)
+                print(f"Maximizing Player: Alpha = {alpha}, Beta = {beta}")
 
                 # If beta is less than or equal to alpha then prune
                 if beta <= alpha:
+                    print("Maximizing Player: Pruned")
                     break
 
-            return best_eval, best_move
+            return max_eval, best_move
 
         # If the player is minimizing then it will return the minimum value
         else:
-            best_eval = float('inf')
+            min_eval = float('inf')
             best_move = None
 
             for move in valid_moves:
                 row, col = move
                 # Make the move
                 self.make_move(row, col)
+                print(f"Minimizing Player: Trying move {move}")
                 # Recursively call minimax for the next level
                 evaluation, _ = self.alphabeta_minimax(
                     depth - 1, True, alpha, beta)
                 # Undo the move
                 self.undo_move()
 
-                if evaluation < best_eval:
-                    best_eval = evaluation
+                if evaluation < min_eval:
+                    min_eval = evaluation
                     best_move = move
 
                 # Take the better between current eval and current beta
                 beta = min(beta, evaluation)
+                print(f"Minimizing Player: Alpha = {alpha}, Beta = {beta}")
 
                 # If beta is less than or equal to alpha then prune
                 if beta <= alpha:
+                    print("Minimizing Player: Pruned")
                     break
 
-            return best_eval, best_move
+            return min_eval, best_move
 
     def evaluate_board(self):
         """Simple evaluation function: difference in piece count"""
-        ai_pieces = sum(row.count(self.current_player) for row in self.board)
-        opponent_pieces = sum(row.count(self.opponent()) for row in self.board)
-        return ai_pieces - opponent_pieces
+        # I changed to my pieces taking the opponen because after making a move
+        # the current player changes
+        # Print the board before evaluating
+        print("Evaluating Board: ")
+        for row in self.board:
+            print(row)
+
+        my_pieces = sum(row.count(self.opponent()) for row in self.board)
+        opponent_pieces = sum(row.count(self.current_player)
+                              for row in self.board)
+        print("AI Pieces: ", my_pieces)
+        print("Opponent Pieces: ", opponent_pieces)
+        return my_pieces - opponent_pieces
 
     def undo_move(self):
         """Undo the move by popping the previous game state from the stack"""
-        if self.move_stack:
-            self.board, self.current_player = self.move_stack.pop()
+        # If we take the top we get the current game state, so we pop the top
+        # and get the top one
+        """ print("Undoing Move")
+        print("Stack before pop: ")
+        for state in self.move_stack:
+            for row in state[0]:
+                print(row)
+            print(state[1])
+            print("-"*10) """
+
+        self.move_stack.pop()
+        # Get the last element of the stack
+        # But important GET A COPY!!!
+        last_state = self.move_stack[-1]
+        self.board = [row[:] for row in last_state[0]]
+        self.current_player = last_state[1]
+
+        # Print the board after undoing the move
+        """ print("Undo Move: ")
+        for row in self.board:
+            print(row)
+        print("Current Player: ", self.current_player)
+        print("-"*10) """
+
+        # Print the stack
+        """ print("Stack after pop: ")
+        for state in self.move_stack:
+            for row in state[0]:
+                print(row)
+            print(state[1])
+            print("-"*10) """
 
     def ai_move(self):
         """Makes a move using the minimax algorithm"""
@@ -255,10 +333,11 @@ class ReversiGame:
         # Make a copy of the state of the game
         # Make a copy of the board because if not it places two pieces
         game_copy = ReversiGame([row[:]
-                                for row in self.board], self.current_player)
+                                for row in self.board], self.current_player,
+                                self.move_stack)
 
         _, move = game_copy.alphabeta_minimax(
-            3, True, float("-inf"), float("inf"))
+            1, True, float("-inf"), float("inf"))
         print("AI Move: ", move)
         row, col = move
         # print("AI Move: ", row, col)
